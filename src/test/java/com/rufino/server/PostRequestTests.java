e49b70b703943f948029204ca0adc68a3efb5581
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-03-15 15:30:18 -0300

    add test

---
+++ src/test/java/com/rufino/server/PostRequestTests.java
@@ -0,0 +1,61 @@
+package com.rufino.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.rufino.server.model.User;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostRequestTests {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    @BeforeEach
+    void clearTable() {
+        jdbcTemplate.update("DELETE FROM user_management");
+    }
+
+    @Test
+    void itShouldSaveUser() throws Exception {
+        JSONObject my_obj = new JSONObject();
+
+        my_obj.put("nickname", "user123");
+        my_obj.put("nome", "John");
+        my_obj.put("lastName", "Doe");
+        my_obj.put("email", "john@gmail.com");
+        my_obj.put("password", "123456");
+
+
+        MvcResult result = mockMvc
+                .perform(post("/api/v1/user/save").contentType(MediaType.APPLICATION_JSON).content(my_obj.toString()))
+                .andExpect(status().isOk()).andReturn();
+
+        User response = objectMapper.readValue(result.getResponse().getContentAsString(), User.class);
+        assertThat(response.getFirstName()).isEqualTo("John");
+        assertThat(response.getLastName()).isEqualTo("Doe");
+        assertThat(response.getEmail()).isEqualTo("john@gmail.com");
+        my_obj = new JSONObject();
+    }
+
+}
