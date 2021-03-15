362bfaa7656b06d90954b053aad7c1a61a3e875d
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-03-15 15:29:18 -0300

    add timestamp

--- src/main/java/com/rufino/server/domain/HttpResponse.java
+++ src/main/java/com/rufino/server/domain/HttpResponse.java
@@ -1,16 +1,26 @@
 package com.rufino.server.domain;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonInclude(Include.NON_NULL)
 @Getter
 @Setter
 public class HttpResponse {
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String message, reason;
+    private ZonedDateTime timestamp;
+    private Map<String, String> errors;
 
     public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
 
@@ -18,9 +28,11 @@ public class HttpResponse {
         this.httpStatus = httpStatus;
         this.message = message;
         this.reason = reason;
+        this.timestamp = ZonedDateTime.now(ZoneId.of("Z"));
     }
 
     public HttpResponse() {
+        this.timestamp = ZonedDateTime.now(ZoneId.of("Z"));
     }
 
 }
