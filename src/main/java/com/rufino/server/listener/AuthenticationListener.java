394d800265c915dd5d1f1e09916ccc2de7ed9ecf
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-03-22 16:24:01 -0300

    correcting mistaken

--- src/main/java/com/rufino/server/listener/AuthenticationListener.java
+++ src/main/java/com/rufino/server/listener/AuthenticationListener.java
@@ -1,12 +1,12 @@
 package com.rufino.server.listener;
 
-import com.rufino.server.model.User;
 import com.rufino.server.services.LoginAttemptService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -30,8 +30,8 @@ public class AuthenticationListener {
     @EventListener
     public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
         Object principal = event.getAuthentication().getPrincipal();
-        if (principal instanceof User) {
-            User user = (User) event.getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            UserDetails user = (UserDetails) event.getAuthentication().getPrincipal();
             loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
         }
 
